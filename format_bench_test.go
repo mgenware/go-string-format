e227134285f02c46be7cd5ccfdc19eb46aad65d0
Mgen <m@mgenware.com>
2019-02-26 21:20:02 +0800

    Add benchmarks

---
+++ format_bench_test.go
@@ -0,0 +1,24 @@
+package strf
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkSprintf(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("🙅‍♂️%v🧛‍♂️%v%v", 1, "haha", -32)
+	}
+}
+
+func BenchmarkFormat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Format("🙅‍♂️{0}🧛‍♂️{1}{2}", 1, "haha", -32)
+	}
+}
+
+func BenchmarkFormatCore(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Format("🙅‍♂️{}🧛‍♂️{}{}", 1, "haha", -32)
+	}
+}
